160a93b5393642a92e3c7e9efc72db85a3955510
Alexandre Signorel <ajsignorel@gmail.com>
2021-03-28 16:22:30 -0400

    changed some settings

--- client.py
+++ client.py
@@ -44,18 +44,18 @@ class GUI():
         #Login Window
         self.login = Toplevel()
         self.login.title("Login")
-        self.login.configure(width = 400, height = 300)
+        self.login.configure(width = 400, height = 215, bg = "black")
         self.login.resizable(width = False, height = False)
 
-        self.pls = Label(self.login, text = "Login to continue", justify = CENTER, font = "Arial 15 bold")
-        self.pls.place(relheight = 0.15, relx = 0.2, rely = 0.07)
+        self.pls = Label(self.login, text = "Login to continue", justify = CENTER, font = "Arial 15 bold", bg = "black", fg = "white")
+        self.pls.place(relheight = 0.15, relx = 0.29, rely = 0.07)
 
-        self.labelName = Label(self.login, text = "Name", font = "Arial 12")
-        self.labelName.place(relheight = 0.2, relx = 0.1, rely = 0.2)
+        self.labelName = Label(self.login, text = "Name:", font = "Arial 13 bold", bg = "black", fg = "white")
+        self.labelName.place(relheight = 0.2, relx = 0.15, rely = 0.15)
 
         #Entry box for typing messages
-        self.entryName = Entry(self.login, font = "Arial 15")
-        self.entryName.place(relwidth = 0.4, relheight = 0.12, relx = 0.35, rely = 0.2)
+        self.entryName = Entry(self.login, font = "Arial 15", bg = "black", fg = "white")
+        self.entryName.place(relwidth = 0.4, relheight = 0.1, relx = 0.3, rely = 0.2)
 
 
         #Curser Focus
@@ -63,7 +63,7 @@ class GUI():
 
         #Continue Button
         self.go = Button(self.login, text = "Continue", font = "Arial 12 bold", command = lambda: self.goAhead(self.entryName.get()))
-        self.go.place(relx = 0.4, rely = 0.55)
+        self.go.place(relx = 0.4, rely = 0.4)
         self.Window.mainloop()
 
     def goAhead(self, name):
@@ -79,26 +79,26 @@ class GUI():
         self.name = name
         self.Window.deiconify()
         self.Window.title("ChatApp")
-        self.Window.configure(width = 470, height = 550, bg = "#17202A")
+        self.Window.configure(width = 470, height = 550, bg = "black")
         self.Window.resizable(width = False, height = False)
-        self.labelHead = Label(self.Window, text = self.name, font = "Arial 11 bold", bg = "#17202A", fg = "#EAECEE", pady = 5)
+        self.labelHead = Label(self.Window, text = self.name, font = "Arial 15 bold", bg = "black", fg = "white", pady = 5)
         self.labelHead.place(relwidth = 1)
-        self.line = Label(self.Window, width = 450, bg = "#ABB2B9")
+        self.line = Label(self.Window, width = 450, bg = "grey")
         self.line.place(relwidth = 1, rely = 0.07, relheight = 0.012)
-        self.textCons = Text(self.Window, width = 20, height = 2, bg = "#17202A", fg = "#EAECEE", font = "Arial 12", padx = 5, pady = 5)
+        self.textCons = Text(self.Window, width = 20, height = 2, bg = "black", fg = "white", font = "Arial 12", padx = 5, pady = 5)
         self.textCons.place(relheight = 0.745, relwidth = 1, rely = 0.08)
-        self.labelBottom = Label(self.Window, bg = "#ABB2B9", height = 80)
+        self.labelBottom = Label(self.Window, bg = "grey", height = 80)
         self.labelBottom.place(relwidth = 1, rely = 0.825)
-        self.entryMsg = Entry(self.labelBottom, bg = "#2C3E50", fg = "#EAECEE", font = "Arial 12")
+        self.entryMsg = Entry(self.labelBottom, bg = "black", fg = "white", font = "Arial 12")
         self.entryMsg.place(relwidth = 0.74, relheight = 0.06, rely = 0.008, relx = 0.011)
 
         self.entryMsg.focus()
 
         #Send Button
-        self.buttonSend = Button(self.labelBottom, text = "Send", font = "Arial 10 bold", width = 20, bg = "#ABB2B9", command = lambda : self.sendButton(self.entryMsg.get()))
+        self.buttonSend = Button(self.labelBottom, text = "Send", font = "Arial 10 bold", width = 20, bg = "grey", command = lambda : self.sendButton(self.entryMsg.get()))
         self.buttonSend.place(relx = 0.77, rely = 0.008, relheight = 0.06, relwidth = 0.22)
 
-        self.textCons.config(cursor = "arrow")
+        self.textCons.config(cursor = "heart")
 
         #Scroll bar
         scrollBar = Scrollbar(self.textCons)
